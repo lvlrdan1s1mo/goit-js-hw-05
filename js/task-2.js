5892515e031d51f56861cffb7b0ee2e25e50e348
lvlrdan1s1mo <danyamalaev1@gmail.com>
2025-01-22 14:46:28 +0100

    Add files via upload

--- js/task-2.js
+++ js/task-2.js
@@ -1,3 +1,6 @@
+const getUsersWithFriend = (users, friendName) => {
+  return users.filter(user => user.friends.includes(friendName))
+}
 const allUsers = [
   {
     name: "Moore Hensley",
@@ -53,5 +56,4 @@ console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
 //   }
 // ]
 
-console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []w
-const getUsersWithFriend = (users, friendName) => users.filter(user => user.includes(friendName))
+console.log(getUsersWithFriend(allUsers, "Adrian Cross" ));
